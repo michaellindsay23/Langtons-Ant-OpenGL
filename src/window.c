e60eb16f6ba2c875fbe74095612a565b62d13d9f
michaellindsay23 <michaelduffielindsay@gmail.com>
2026-01-27 10:17:29 -0500

    window display

--- src/window.c
+++ src/window.c
@@ -4,6 +4,12 @@
 #include "GL/gl.h"
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
 int main(void) {
   GLFWwindow* window;
 
@@ -14,18 +20,27 @@ int main(void) {
   // Create the window
   window = glfwCreateWindow(640, 480, "Ant", NULL, NULL);
   if (!window) {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
 
   glfwMakeContextCurrent(window);
 
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    //glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(window);
-
     glfwPollEvents();
   }
 
